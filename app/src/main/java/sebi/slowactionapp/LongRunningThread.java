5c94ced8bc65fd3d79db552cac7dafe40b4e15dc
sebi90 <sebwie90@googlemail.com>
2015-05-30 20:25:52 +0200

    Initial commit

---
+++ app/src/main/java/sebi/slowactionapp/LongRunningThread.java
@@ -0,0 +1,44 @@
+package sebi.slowactionapp;
+
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Sebi on 30.05.15.
+ */
+public class LongRunningThread extends Thread {
+
+    private long total;
+    private EditText input;
+    private TextView output;
+    private String format;
+
+    public LongRunningThread(long total, EditText input, TextView output, String format) {
+        this.total = total;
+        this.input = input;
+        this.output = output;
+        this.format = format;
+    }
+
+    public void run() {
+        // Wird einmal von Thread-xxx aufgerufen
+        Log.d("run_LongRunningThread", Thread.currentThread().getName());
+        long rest = total;
+        while (rest > 0) {
+            long thisTime = Math.min(rest, 1000L);
+
+            try {
+                Thread.sleep(thisTime);
+            }
+            catch (Exception e) {}
+
+            rest -= thisTime;
+            Request req = new Request(input, "" + rest);
+            input.post(req);
+        }
+        String message = String.format(format, total);
+        Request req = new Request(output, message);
+        input.post(req);
+    }
+}
